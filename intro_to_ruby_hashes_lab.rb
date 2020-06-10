34df86e6d43e64f96ca0a0558fbd204e9eb589db
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-10 21:10:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -56,7 +56,7 @@ def monopoly_with_fourth_tier
               },
               :names => {
                 :reading_railroad => {
-                  :mortgage_value => 100
+                  :mortgage_value => 200
                 },
                 :pennsylvania_railroad => {},
                 :b_and_o_railroad => {},
